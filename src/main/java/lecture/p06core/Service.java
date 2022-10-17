d756388b39ae5a999891c887f8fb7ef4268bb5a6
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-17 17:43:06 +0900

    생성자 통한 주입

---
+++ src/main/java/lecture/p06core/Service.java
@@ -0,0 +1,15 @@
+package lecture.p06core;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Service {
+	private Dao dao;
+	
+	
+	
+}
