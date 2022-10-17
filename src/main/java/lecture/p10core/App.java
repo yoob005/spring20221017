9c3ae1450d6aed12ad7179d89dbf91a444109480
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-17 17:45:25 +0900

    컴포넌트 스캔으로 컴포넌트 bean 다 찾음

---
+++ src/main/java/lecture/p10core/App.java
@@ -0,0 +1,21 @@
+package lecture.p10core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		String config = "/lecture/p10core/context.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(config);
+		
+		Controller c = context.getBean(Controller.class);
+		Service s = context.getBean(Service.class);
+		
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println(c.getService());
+		
+		
+		System.out.println("프로그램 계속 실행");
+	}
+}
