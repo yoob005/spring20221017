2e1613db16f42b852b890b66201a960e0649824b
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-18 16:39:45 +0900

    spring base

---
+++ src/main/java/lecture/p13core/Controller.java
@@ -0,0 +1,10 @@
+package lecture.p13core;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Controller {
+	private Dao dao;
+}
