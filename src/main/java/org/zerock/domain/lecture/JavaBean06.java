07caf6751b0384db1437eeafcb219a68253822d3
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-26 17:08:05 +0900

    자바빈 파일

---
+++ src/main/java/org/zerock/domain/lecture/JavaBean06.java
@@ -0,0 +1,10 @@
+package org.zerock.domain.lecture;
+
+import lombok.Data;
+
+@Data
+public class JavaBean06 {
+
+	private String name;
+	private double price;
+}
---
+++ src/main/java/org/zerock/domain/lecture/JavaBean11.java
@@ -0,0 +1,13 @@
+package org.zerock.domain.lecture;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class JavaBean11 {
+	
+	private int id;
+	private String name;
+	private List<String> productName;
+}
