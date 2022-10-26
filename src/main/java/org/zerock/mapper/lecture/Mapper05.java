e9cfbbeee5cf7a7815cb9798a7ee368938579388
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-26 17:08:33 +0900

    1026 복잡한 결과 매핑하기

---
+++ src/main/java/org/zerock/mapper/lecture/Mapper05.java
@@ -0,0 +1,14 @@
+package org.zerock.mapper.lecture;
+
+import org.zerock.domain.lecture.JavaBean07;
+import org.zerock.domain.lecture.JavaBean08;
+import org.zerock.domain.lecture.JavaBean09;
+import org.zerock.domain.lecture.JavaBean10;
+
+public interface Mapper05 {
+	JavaBean07 getOrder();
+	JavaBean08 getOrder2();
+	
+	JavaBean09 getProduct();
+	JavaBean10 getSupplier();
+}
