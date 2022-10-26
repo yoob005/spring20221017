275b32934c4c85e97d11f8fc3611d3620414d8bc
yoob005 <110441590+yoob005@users.noreply.github.com>
2022-10-26 17:07:54 +0900

    단순한 결과 매핑하기.

--- src/main/java/org/zerock/mapper/lecture/Mapper01.java
+++ src/main/java/org/zerock/mapper/lecture/Mapper01.java
@@ -1,10 +1,27 @@
 package org.zerock.mapper.lecture;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.apache.ibatis.annotations.Select;
 
 public interface Mapper01 {
 	
 	@Select("SELECT NOW()")
 	public String getCurrentTime();
+	
+	@Select("SELECT CustomerName FROM w3schools.Customers WHERE CustomerId = 77")
+	public String getCustomerName();
+	
+	public String getEmployeeName();
+	
+	public String getProductName();
+	
+	public double getPrice();
+	
+	public int getQuantity();
+	
+	public LocalDate getBirthDate();
+	
 	
 }
---
+++ src/main/java/org/zerock/mapper/lecture/Mapper04.java
@@ -0,0 +1,13 @@
+package org.zerock.mapper.lecture;
+
+import java.util.List;
+
+import org.zerock.domain.lecture.JavaBean05;
+import org.zerock.domain.lecture.JavaBean06;
+
+public interface Mapper04 {
+
+	List<JavaBean06> getProductList();
+	
+	List<JavaBean05> getEmployeeList();
+}
